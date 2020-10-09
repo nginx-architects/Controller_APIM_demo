5e57e15078815fd45581aeaaa8bec81876778b82
Shouvik <shouvik.dutta96@gmail.com>
2020-10-09 16:35:03 -0500

    testing out Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,11 +2,13 @@ pipeline {
     agent any
 
     stages {
-        stage('Hello') {
+        stage('Login to Controller') {
             steps {
+                sh '''
                 echo 'Hello World'
                 node --version
                 npm --version
+                '''
             }
         }
     }
