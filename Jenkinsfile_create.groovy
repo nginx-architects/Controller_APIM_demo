d41dcbb466b14aa1fe0c369623de4531a8b3fd1a
Shouvik <shouvik.dutta96@gmail.com>
2020-10-12 14:27:56 -0500

    added slack notification logic

--- Jenkinsfile_create.groovy
+++ Jenkinsfile_create.groovy
@@ -1,4 +1,12 @@
+def COLOR_MAP = ['SUCCESS': 'good', 'FAILURE': 'danger', 'UNSTABLE': 'danger', 'ABORTED': 'danger']
+
+def getBuildUser() {
+  return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+}
 pipeline {
+    environment {
+        BUILD_USER = ''
+    }
     agent any
 
     parameters {
@@ -20,6 +28,16 @@ pipeline {
                 set -x
                 '''
             }
+        }
+    }
+    stage('Slack Message') {
+        steps {
+            script {
+                BUILD_USER = getBuildUser()
+            }
+            slackSend channel: '#apim-garage-day',
+            color: COLOR_MAP[currentBuild.currentResult],
+            message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} by ${BUILD_USER}\n More info at: ${env.BUILD_URL}"
         }
     }
 }
