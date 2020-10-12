cadff5627d15f8697744562b6d33d54bae56ab92
Shouvik <shouvik.dutta96@gmail.com>
2020-10-11 21:18:46 -0500

    added parameters to the jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,19 @@
 pipeline {
     agent any
 
+    parameters {
+        string(defaultValue: "10.1.1.6", description: 'Controller (UDF URL / IP Address)', name: 'controllerUrl')
+        string(defaultValue: "admin@test.com", description: 'Controller Username', name: 'username')
+        password(description: 'Controller Password', name: 'password')
+    }
+
     tools {nodejs "node"}
 
     stages {
         stage('Login to Controller') {
             steps {
                 sh '''
-                echo 'Hello World'
-                ./Scripts/login.sh
+                ./Scripts/login.sh $controllerUrl $username $password
                 '''
             }
         }
--- Scripts/login.sh
+++ Scripts/login.sh
@@ -1,2 +1,5 @@
 echo 'Inside login'
+echo $1
+echo $2
+echo $3
 newman run -k ./PostmanCollections/Controller3.x_demo_login.postman_collection.json -e  ./PostmanCollections/Controller3.x_Env.postman_environment.json
