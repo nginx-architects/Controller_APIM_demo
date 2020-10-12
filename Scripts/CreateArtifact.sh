bcbe6d9acd4a066a07e2d5648c424befcd4cc461
Shouvik <shouvik.dutta96@gmail.com>
2020-10-12 15:22:57 -0500

    refactory

--- Scripts/CreateArtifact.sh
+++ Scripts/CreateArtifact.sh
@@ -1,5 +1,5 @@
 echo "Running Create Artifacts Script"
 sed "s/controller_host/$1/;s/controller_username/$2/;s/controller_password/$3/" ./PostmanCollections/Controller3.x_Env.postman_environment.json > temp_env.json
 
-newman run -k ./PostmanCollections/Controller3.x_demo_prep.postman_collection.json -e  ./temp_env.json --bail newman
+newman run -k ./PostmanCollections/Controller3.x_demo_prep.postman_collection.json -e  ./temp_env.json --bail
 rm ./temp_env.json
