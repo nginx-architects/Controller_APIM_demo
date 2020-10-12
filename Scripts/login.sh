707c6b7410972a4d1dda39fb872a94b045e9f11c
Shouvik <shouvik.dutta96@gmail.com>
2020-10-12 09:43:22 -0500

    refactory

--- Scripts/login.sh
+++ Scripts/login.sh
@@ -1,9 +1,7 @@
 echo $1
 echo $2
 echo $3
-sed 's/controller_host/$1/' ./PostmanCollections/Controller3.x_Env.postman_environment.json
-sed 's/controller_username/$2/' ./PostmanCollections/Controller3.x_Env.postman_environment.json
-sed 's/controller_password/$3/' ./PostmanCollections/Controller3.x_Env.postman_environment.json
+sed "s/controller_host/$1/;s/controller_username/$2/;s/controller_password/$3/" ./PostmanCollections/Controller3.x_Env.postman_environment.json
 cat ./PostmanCollections/Controller3.x_Env.postman_environment.json
 
 newman run -k ./PostmanCollections/Controller3.x_demo_login.postman_collection.json -e  ./PostmanCollections/Controller3.x_Env.postman_environment.json
