045f25633e49c5222eeba3165dfa80ab9c5935d5
Shouvik <shouvik.dutta96@gmail.com>
2020-10-22 09:36:53 -0500

    updated cleanup script to send an exit command if newman fails

--- Scripts/CleanupArtifact.sh
+++ Scripts/CleanupArtifact.sh
@@ -2,4 +2,10 @@ echo "Running Cleanup Artifact Script"
 sed "s/controller_host/$1/;s/controller_username/$2/;s/controller_password/$3/" ./PostmanCollections/Controller3.x_Env.postman_environment.json > temp_env.json
 
 newman run -k ./PostmanCollections/1_Controller3.x_demo_cleanup.postman_collection.json -e  ./temp_env.json --bail
+newman_output=$?
 rm ./temp_env.json
+if [ $newman_output -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
